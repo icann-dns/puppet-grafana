8330f173287b9ff506f0d9eb1cfe046971f08661
jbond <github@johnbond.org>
2024-01-11 18:34:49 +0100

    service: update code to remove away from create_resources
    This allows us to correctly notify the service
    
    DNSIN-7

--- manifests/settings.pp
+++ manifests/settings.pp
@@ -3,8 +3,10 @@ define grafana::settings (
   Hash[String, Any, 1] $settings,
   String $section = $title,
 ) {
+  include grafana
   $ini_defaults = {
-    path => $::grafana::config::file,
+    path   => $grafana::config::file,
+    notify => Service[$grafana::service_name],
   }
 
   $_settings = delete_undef_values($settings)
